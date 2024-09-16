2108e36ef08b177bf4654a04666d2ce7f750841a
u-na-gi <orcinusorca1758dv6932@gmail.com>
2024-09-16 11:31:11 +0900

    does not support interactive transaction.

--- deployment-platforms/edge/cloudflare-workers/with-d1/prisma/schema.sql
+++ deployment-platforms/edge/cloudflare-workers/with-d1/prisma/schema.sql
@@ -8,3 +8,12 @@ CREATE TABLE "User" (
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
+CREATE TABLE TOKEN (
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	token TEXT NOT NULL,
+	user_id INTEGER NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES User(id)
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_token_key" ON "TOKEN"("token", "user_id");
